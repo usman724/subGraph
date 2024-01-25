8382fedae1083b368f9a7d8b412e2e251ea01d0d
usman Pervaiz <usmanpervaiz724@gmail.com>
2024-01-25 11:31:26 +0500

    add

---
+++ src/utils/constants.ts
@@ -0,0 +1,19 @@
+/* eslint-disable prefer-const */
+import { BigInt, BigDecimal, Address } from '@graphprotocol/graph-ts'
+import { Factory as FactoryContract } from '../types/templates/Pool/Factory'
+
+export const ADDRESS_ZERO = '0x0000000000000000000000000000000000000000'
+export const FACTORY_ADDRESS = '0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0'
+
+
+
+
+
+export let ZERO_BI = BigInt.fromI32(0)
+export let ONE_BI = BigInt.fromI32(1)
+export let ZERO_BD = BigDecimal.fromString('0')
+export let ONE_BD = BigDecimal.fromString('1')
+export let BI_18 = BigInt.fromI32(18)
+
+export let factoryContract = FactoryContract.bind(Address.fromString(FACTORY_ADDRESS))
+
